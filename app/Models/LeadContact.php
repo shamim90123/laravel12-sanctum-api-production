408ce2fd340e3c4918e27b3f7d981607c7ac9b45
shamim reza <shamimreza@revinr.com>
2025-10-30 14:22:21 +0600

    add contact option added in list modal

--- app/Models/LeadContact.php
+++ app/Models/LeadContact.php
@@ -11,13 +11,19 @@ class LeadContact extends Model
 
     // protected $fillable = ['name', 'email', 'phone', 'job_title', 'department', 'primary_status', 'lead_id'];
     protected $fillable = [
-    'lead_id', 'first_name', 'last_name', 'name', 'email', 'phone', 'job_title', 'department',
-    'primary_status', // legacy
-    'is_primary', 'item_id', 'link'
-];
+        'lead_id', 'first_name', 'last_name', 'name', 'email', 'phone', 'job_title', 'department',
+        'primary_status', // legacy
+        'is_primary', 'item_id', 'link'
+    ];
 
     public function lead()
     {
         return $this->belongsTo(Lead::class);
+    }
+
+    public function products()
+    {
+        // Example if pivot table is lead_contact_product or contact_product
+        return $this->belongsToMany(Product::class, 'lead_products', 'contact_id', 'product_id');
     }
 }
